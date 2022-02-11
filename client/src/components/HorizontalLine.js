66cd1b0273d9c8db95b813344003915a4c62984f
Jun Lee <lejhn1@gmail.com>
2022-02-10 19:41:44 -0600

    FRONT-END UPDATED

--- client/src/components/HorizontalLine.js
+++ client/src/components/HorizontalLine.js
@@ -4,14 +4,15 @@ const HorizonLine = ({ text }) => {
   return (
     <div
       style={{
-        width: "73%",
+        width: "60%",
         textAlign: "center",
         borderBottom: "2px solid #aaa",
         lineHeight: "0.1em",
         margin: "75px 0 35px",
       }}
     >
-      <span style={{ background: "#fff", padding: "0 30px", fontSize: "30px", fontFamily: "Trebuchet MS" }}>{text}</span>
+      <span style={{ background: "#fff", padding: "0 30px", fontSize: "30px", fontFamily: "Trebuchet MS",
+      fontWeight: "bold"}}>{text}</span>
     </div>
   );
 };
